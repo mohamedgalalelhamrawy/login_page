6af982db2b34fa3741c9c0892a607f382ace4e1b
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-08-25 11:30:34 +0300

    add button of login to home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,21 +16,38 @@ class HomePage extends StatelessWidget {
             SizedBox(
               height: MediaQuery.sizeOf(context).height * 0.1,
             ),
-            Text(
+            const  Text(
               "Log In",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 28,
                   fontWeight: FontWeight.bold),
             ),
-            SizedBox(
+           const  SizedBox(
               height: 35,
             ),
             CustomTextField(text: "User Name", icon: Icon(Icons.person)),
-            SizedBox(
+           const  SizedBox(
               height: 23,
             ),
             CustomTextField(text: "Password", icon: Icon(Icons.lock),suffixicon: Icon(Icons.visibility),),
+             const  SizedBox(
+              height: 25,
+            ),
+            GestureDetector(
+              child: Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color(0xffEE3A57)
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text("Log In",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+                ),
+              ),
+            )
           ],
         ),
       ),
